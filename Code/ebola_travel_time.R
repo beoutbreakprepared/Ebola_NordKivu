2f452fdc42e0c0a9adc21edece48b0862c89c96d
Julia <morgaj5@uw.edu>
2019-07-17 17:24:46 -0700

    SR 50 update

--- Code/ebola_travel_time.R
+++ Code/ebola_travel_time.R
@@ -26,6 +26,7 @@ source('Code/plot_funcs.R')
 
 # Read in data
 health_areas <- shapefile('Data/drc_health_areas.shp') #health area shapefile with binary present/absence variable for infection based on WHO SitRep 42 from May 21, 2019
+ariwara_zone <- shapefile('Data/ariwara.shp')
 uga_parish <- shapefile('Data/Uganda_parish_crop.shp')
 
 
@@ -48,11 +49,11 @@ coordinates(ug_points) <- ~ Longitude + Latitude
 proj4string(ug_points) <- proj4string(health_areas)
 
 ug_infect <- raster::intersect(uga_parish, ug_points)
-ug_infect$sr_49_new <- 1
+ug_infect$sr_50_new <- 1
 
 
-health_areas <- bind(health_areas, ug_infect)
-ha_infect <- health_areas[which(health_areas@data$sr_49_new == 1),]
+health_areas <- bind(health_areas, ug_infect, ariwara_zone)
+ha_infect <- health_areas[which(health_areas@data$sr_50_new == 1),]
 
 
 # Sample infected health areas with population bias, if pop or area too small use spsample
@@ -77,10 +78,6 @@ for (i in 1:length(ha_infect)){
   poly_samp <- rbind(poly_samp, pts)
 }
 
-# For Ariwara case (not included in WHO shapefile)
-ariwara <- ariwara <- data.frame(long = 30.70048, lat = 3.136048) # coordinates extracted from Google maps
-poly_samp <- rbind(poly_samp, ariwara)
-
 # Format matrix of points to use in calculating travel time
 dataset <- poly_samp
 coordinates(dataset) <- ~ long + lat
@@ -91,12 +88,12 @@ points <- as.matrix(dataset@coords)
 Tr <- transition(friction, function(x) 1/mean(x), 8)
 T.GC <- geoCorrection(Tr)
 access.raster <- accCost(x = T.GC, fromCoords =  points)
-writeRaster(access.raster, filename = 'Outputs_SR49/access_raster_raw.tif')
+writeRaster(access.raster, filename = 'Outputs_SR50/access_raster_raw.tif', overwrite = TRUE)
 
 # Convert infinite values to NA and format values to show hours
 values(access.raster)[which(is.infinite(values(access.raster)))] <- NA
 values(access.raster) <- values(access.raster)/60
-writeRaster(access.raster, filename = 'Outputs_SR49/access_raster_hours.tif')
+writeRaster(access.raster, filename = 'Outputs_SR50/access_raster_hours.tif', overwrite = TRUE)
 
 # Plot access raster data
 adm0 <- getShp(ISO = c("UGA", "COD", "RWA", "BDI", "TZA", "SSD", "CAF", "ETH", "SDN"), admin_level = 'admin0')
@@ -112,10 +109,10 @@ ug_plot <- plot_rel_map(infected_sf = ha_infect, access_raster = access.raster, 
 sd_plot <- plot_rel_map(infected_sf = ha_infect, access_raster = access.raster, bg_adm0 = adm0, lakes = lakes, co = 'SSD')
 
 # To save plots use
-ggsave(trav_time_plot, filename = 'Outputs_SR49/Travel_time_map.png', width = 8, height = 8)
-ggsave(rw_plot, filename = 'Outputs_SR49/Rwanda_map.png', width = 8, height = 8)
-ggsave(ug_plot, filename = 'Outputs_SR49/Uganda_map.png', width = 8, height = 8)
-ggsave(sd_plot, filename = 'Outputs_SR49/SSudan_map.png', width = 8, height = 8)
+ggsave(trav_time_plot, filename = 'Outputs_SR50/Travel_time_map.png', width = 8, height = 8)
+ggsave(rw_plot, filename = 'Outputs_SR50/Rwanda_map.png', width = 8, height = 8)
+ggsave(ug_plot, filename = 'Outputs_SR50/Uganda_map.png', width = 8, height = 8)
+ggsave(sd_plot, filename = 'Outputs_SR50/SSudan_map.png', width = 8, height = 8)
 
 # For DRC, Rwanda and Uganda, create a list of hospital travel times
 # Rbind all country hospitals and correct names
@@ -136,11 +133,11 @@ ug_hosp_tt <- hospitals[order(Travel_Time),][Country == 'Uganda',][1:20, .(Admin
 ss_hosp_tt <- hospitals[order(Travel_Time),][Country == 'South Sudan',][1:20, .(Admin1, Name, Travel_Time, Longitude, Latitude)]
 
 # Save to csv
-write.csv(hospitals, file = file('Outputs_SR49/hospital_tt.csv', encoding = 'UTF-8'))
-write.csv(drc_hosp_tt, file = file('Outputs_SR49/drc_hosp_tt.csv', encoding = 'UTF-8'))
-write.csv(rw_hosp_tt, file = file('Outputs_SR49/rw_hosp_tt.csv', encoding = 'UTF-8'))
-write.csv(ug_hosp_tt, file = file('Outputs_SR49/ug_hosp_tt.csv', encoding = 'UTF-8'))
-write.csv(ss_hosp_tt, file = file('Outputs_SR49/ss_hosp_tt.csv', encoding = 'UTF-8'))
+write.csv(hospitals, file = file('Outputs_SR50/hospital_tt.csv', encoding = 'UTF-8'))
+write.csv(drc_hosp_tt, file = file('Outputs_SR50/drc_hosp_tt.csv', encoding = 'UTF-8'))
+write.csv(rw_hosp_tt, file = file('Outputs_SR50/rw_hosp_tt.csv', encoding = 'UTF-8'))
+write.csv(ug_hosp_tt, file = file('Outputs_SR50/ug_hosp_tt.csv', encoding = 'UTF-8'))
+write.csv(ss_hosp_tt, file = file('Outputs_SR50/ss_hosp_tt.csv', encoding = 'UTF-8'))
 
 # Save subset of columns to table output
 drc_tab <- tableGrob(drc_hosp_tt[,.(District = Admin1, Name, `Travel Time (hours)` = round(Travel_Time, 2))], rows = NULL)
@@ -148,7 +145,7 @@ rw_tab <- tableGrob(rw_hosp_tt[,.(District = Admin1, Name, `Travel Time (hours)`
 ug_tab <- tableGrob(ug_hosp_tt[,.(District = Admin1, Name, `Travel Time (hours)` = round(Travel_Time, 2))], rows = NULL)
 ss_tab <- tableGrob(ss_hosp_tt[,.(District = Admin1, Name, `Travel Time (hours)` = round(Travel_Time, 2))], rows = NULL)
 
-ggsave(drc_tab, filename = 'Outputs_SR49/drc_hosp_tt_prettytab.png')
-ggsave(rw_tab, filename = 'Outputs_SR49/rw_hosp_tt_prettytab.png')
-ggsave(ug_tab, filename = 'Outputs_SR49/ug_hosp_tt_prettytab.png')
-ggsave(ss_tab, filename = 'Outputs_SR49/ss_hosp_tt_prettytab.png')
+ggsave(drc_tab, filename = 'Outputs_SR50/drc_hosp_tt_prettytab.png', height = 8, width = 4)
+ggsave(rw_tab, filename = 'Outputs_SR50/rw_hosp_tt_prettytab.png', height = 8, width = 4)
+ggsave(ug_tab, filename = 'Outputs_SR50/ug_hosp_tt_prettytab.png', height = 8, width = 4)
+ggsave(ss_tab, filename = 'Outputs_SR50/ss_hosp_tt_prettytab.png', height = 8, width = 4)
